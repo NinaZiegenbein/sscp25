7ef83aea5b5c08fd65fa87b09d098618faa43c44
Ingvild <ingvildadde@gmail.com>
2025-08-07 16:11:06 -0700

    Added fibrosis analysis

--- analyze_modes.py
+++ analyze_modes.py
@@ -5,10 +5,10 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 from scipy import stats
 import os
-CLINICAL_DATA_XL = "../ClinicalData_OUS_MADPatients_EIVIND_29_4_2021.xlsx"
-PCA_SCORES_CSV = "./PCA_Results_final_height/pca_scores.csv"
-NUM_MODES = 10
-ANALYSIS_MODES = [f"M{i}" for i in range(1, NUM_MODES + 1)]
+# CLINICAL_DATA_XL = "../ClinicalData_OUS_MADPatients_EIVIND_29_4_2021.xlsx"
+# PCA_SCORES_CSV = "./PCA_Results_final_height/pca_scores.csv"
+# NUM_MODES = 10
+# ANALYSIS_MODES = [f"M{i}" for i in range(1, NUM_MODES + 1)]
 
 
 def load_patient_data(file_path):
@@ -43,75 +43,181 @@ def annotate_p_values(ax, p_values, y_pos=3, color="black"):
         ax.annotate(f"P = {p_val:.2f}", (i - 0.4, y_pos), color=color)
 
 
-clinical_data = load_patient_data(CLINICAL_DATA_XL)
+def run_analysis_arrhythmia(clinical_data_path, pca_scores_path, num_modes):
 
-pca_scores = load_pca_scores(PCA_SCORES_CSV)
+    CLINICAL_DATA_XL = clinical_data_path
+    PCA_SCORES_CSV = pca_scores_path
+    NUM_MODES = num_modes
+    ANALYSIS_MODES = [f"M{i}" for i in range(1, NUM_MODES + 1)]
 
-if clinical_data is not None and pca_scores is not None:
-   
-    clinical_data["arrhythmic_composite"] = (
-        clinical_data[["Aborted_cardiac_arrest", "Ventricular_tachycardia"]].fillna(0).sum(axis=1) > 0
-    )
-    clinical_data["any_arrhythmia"] = (
-        clinical_data[["Aborted_cardiac_arrest", "Ventricular_tachycardia", "nsVT"]].fillna(0).sum(axis=1) > 0
-    )
+    clinical_data = load_patient_data(CLINICAL_DATA_XL)
 
+    pca_scores = load_pca_scores(PCA_SCORES_CSV)
+
+    if clinical_data is not None and pca_scores is not None:
     
-    pca_clinical = pca_scores.merge(clinical_data, on="Pat_no")
+        clinical_data["arrhythmic_composite"] = (
+            clinical_data[["Aborted_cardiac_arrest", "Ventricular_tachycardia"]].fillna(0).sum(axis=1) > 0
+        )
+        clinical_data["any_arrhythmia"] = (
+            clinical_data[["Aborted_cardiac_arrest", "Ventricular_tachycardia", "nsVT"]].fillna(0).sum(axis=1) > 0
+        )
+
+        
+        pca_clinical = pca_scores.merge(clinical_data, on="Pat_no")
+        
+        pca_clinical_event = pca_clinical[pca_clinical["arrhythmic_composite"]]
+        pca_clinical_noevent = pca_clinical[~pca_clinical["any_arrhythmia"]]
+        
+        any_arrhythmia = pca_clinical["any_arrhythmia"].sum()
+
+        # Overlap between categories
+        nsvt_aca_overlap = ((pca_clinical["nsVT"] == 1) & (pca_clinical["Aborted_cardiac_arrest"] == 1)).sum()
+        vt_aca_overlap = ((pca_clinical["Ventricular_tachycardia"] == 1) & (pca_clinical["Aborted_cardiac_arrest"] == 1)).sum()
+        vt_nsvt_overlap = ((pca_clinical["Ventricular_tachycardia"] == 1) & (pca_clinical["nsVT"] == 1)).sum()
+        all_three = ((pca_clinical["nsVT"] == 1) & (pca_clinical["Aborted_cardiac_arrest"] == 1) & (pca_clinical["Ventricular_tachycardia"] == 1)).sum()
+
+        print(f"nsVT and ACA overlap: {nsvt_aca_overlap}")
+        print(f"VT and ACA overlap: {vt_aca_overlap}")
+        print(f"VT and nsVT overlap: {vt_nsvt_overlap}")
+        print(f"All three overlap: {all_three}")
+        
+        # Statistical Analysis
+        mann_whitney_p_values = calculate_mann_whitney(
+            pca_clinical_event, pca_clinical_noevent, ANALYSIS_MODES
+        )
+        print(pd.DataFrame([mann_whitney_p_values], columns=ANALYSIS_MODES, index=["p-value"]))
+
+        # Prepare Data for Plotting
+        pca_scores_stacked = pca_scores[ANALYSIS_MODES].stack().reset_index(level=1)
+        pca_scores_stacked.columns = ["mode", "score"]
+        clinical_data_pca_stacked = clinical_data.merge(pca_scores_stacked, on="Pat_no")
+
+        # Plotting
+        plt.rcParams.update({"font.size": 14})
+        fig, ax = plt.subplots(figsize=(12, 6))
+        sns.boxplot(
+            data=clinical_data_pca_stacked,
+            x="mode",
+            y="score",
+            hue="arrhythmic_composite",
+            ax=ax,
+            palette="Set2",
+        )
+
+
+        ax.set_ylim(-3, 5)
+        annotate_p_values(ax, mann_whitney_p_values, y_pos=4)
+        ax.set_xlabel("Patient Mode Score (Standardized)")
+        ax.set_ylabel("PCA Score")
+        # ax.legend(title="Arrhythmic Composite", loc="upper center")
+        ax.legend(
+            title="Arrhythmic Composite",
+            loc="upper center",
+            bbox_to_anchor=(0.5, 1.2),  # adjust vertical offset as needed
+            ncol=2  # layout legend in a row if many labels
+        )
+
+        plt.tight_layout()
+        PCA_folder = PCA_SCORES_CSV.rsplit('/', 1)[0]
+        figure_path = os.path.join(PCA_folder, "figures")
+        if not os.path.exists(figure_path):
+            os.makedirs(figure_path)
+        plt.savefig(os.path.join(figure_path, "arrhythmia_mode_comparison.svg"))
+        plt.show()
+
+
+def run_analysis_fibrosis(clinical_data_path, pca_scores_path, num_modes):
+
+    CLINICAL_DATA_XL = clinical_data_path
+    PCA_SCORES_CSV = pca_scores_path
+    NUM_MODES = num_modes
+    ANALYSIS_MODES = [f"M{i}" for i in range(1, NUM_MODES + 1)]
+
+    clinical_data = load_patient_data(CLINICAL_DATA_XL)
+
+    pca_scores = load_pca_scores(PCA_SCORES_CSV)
+
+    if clinical_data is not None and pca_scores is not None:
+
+        fibrosis_columns = ['CMR_LGE_Myocardium_Y_N',
+                            'CMR_LGE_Myo_adjacent',
+                            'CMR_LGE_PM_Y_N',
+                            'CMR_LGE_ANT_Pap_muscle',
+                            'CMR_LGE_POST_Pap_muscle',
+                            'LGE_MV_Hopp',
+                            'LGE_chordae_Hopp']
     
-    pca_clinical_event = pca_clinical[pca_clinical["arrhythmic_composite"]]
-    pca_clinical_noevent = pca_clinical[~pca_clinical["any_arrhythmia"]]
-    
-    any_arrhythmia = pca_clinical["any_arrhythmia"].sum()
+        # clinical_data["arrhythmic_composite"] = (
+        #     clinical_data[["Aborted_cardiac_arrest", "Ventricular_tachycardia"]].fillna(0).sum(axis=1) > 0
+        # )
+        clinical_data["any_fibrosis"] = (
+            clinical_data[fibrosis_columns].fillna(0).sum(axis=1) > 0
+        )
 
-    # Overlap between categories
-    nsvt_aca_overlap = ((pca_clinical["nsVT"] == 1) & (pca_clinical["Aborted_cardiac_arrest"] == 1)).sum()
-    vt_aca_overlap = ((pca_clinical["Ventricular_tachycardia"] == 1) & (pca_clinical["Aborted_cardiac_arrest"] == 1)).sum()
-    vt_nsvt_overlap = ((pca_clinical["Ventricular_tachycardia"] == 1) & (pca_clinical["nsVT"] == 1)).sum()
-    all_three = ((pca_clinical["nsVT"] == 1) & (pca_clinical["Aborted_cardiac_arrest"] == 1) & (pca_clinical["Ventricular_tachycardia"] == 1)).sum()
+        
+        pca_clinical = pca_scores.merge(clinical_data, on="Pat_no")
+        
+        # pca_clinical_event = pca_clinical[pca_clinical["arrhythmic_composite"]]
+        pca_clinical_event = pca_clinical[pca_clinical["any_fibrosis"]]
+        pca_clinical_noevent = pca_clinical[~pca_clinical["any_fibrosis"]]
 
-    print(f"nsVT and ACA overlap: {nsvt_aca_overlap}")
-    print(f"VT and ACA overlap: {vt_aca_overlap}")
-    print(f"VT and nsVT overlap: {vt_nsvt_overlap}")
-    print(f"All three overlap: {all_three}")
-    
-    # Statistical Analysis
-    mann_whitney_p_values = calculate_mann_whitney(
-        pca_clinical_event, pca_clinical_noevent, ANALYSIS_MODES
-    )
-    print(pd.DataFrame([mann_whitney_p_values], columns=ANALYSIS_MODES, index=["p-value"]))
+        any_fibrosis = pca_clinical["any_fibrosis"].sum()
 
-    # Prepare Data for Plotting
-    pca_scores_stacked = pca_scores[ANALYSIS_MODES].stack().reset_index(level=1)
-    pca_scores_stacked.columns = ["mode", "score"]
-    clinical_data_pca_stacked = clinical_data.merge(pca_scores_stacked, on="Pat_no")
+        # Overlap between categories
+        # nsvt_aca_overlap = ((pca_clinical["nsVT"] == 1) & (pca_clinical["Aborted_cardiac_arrest"] == 1)).sum()
+        # vt_aca_overlap = ((pca_clinical["Ventricular_tachycardia"] == 1) & (pca_clinical["Aborted_cardiac_arrest"] == 1)).sum()
+        # vt_nsvt_overlap = ((pca_clinical["Ventricular_tachycardia"] == 1) & (pca_clinical["nsVT"] == 1)).sum()
+        # all_three = ((pca_clinical["nsVT"] == 1) & (pca_clinical["Aborted_cardiac_arrest"] == 1) & (pca_clinical["Ventricular_tachycardia"] == 1)).sum()
 
-    # Plotting
-    plt.rcParams.update({"font.size": 14})
-    fig, ax = plt.subplots(figsize=(12, 6))
-    sns.boxplot(
-        data=clinical_data_pca_stacked,
-        x="mode",
-        y="score",
-        hue="arrhythmic_composite",
-        ax=ax,
-        palette="Set2",
-    )
+        # print(f"nsVT and ACA overlap: {nsvt_aca_overlap}")
+        # print(f"VT and ACA overlap: {vt_aca_overlap}")
+        # print(f"VT and nsVT overlap: {vt_nsvt_overlap}")
+        # print(f"All three overlap: {all_three}")
+        
+        # Statistical Analysis
+        mann_whitney_p_values = calculate_mann_whitney(
+            pca_clinical_event, pca_clinical_noevent, ANALYSIS_MODES
+        )
+        print(pd.DataFrame([mann_whitney_p_values], columns=ANALYSIS_MODES, index=["p-value"]))
 
+        # Prepare Data for Plotting
+        pca_scores_stacked = pca_scores[ANALYSIS_MODES].stack().reset_index(level=1)
+        pca_scores_stacked.columns = ["mode", "score"]
+        clinical_data_pca_stacked = clinical_data.merge(pca_scores_stacked, on="Pat_no")
 
-    ax.set_ylim(-3, 5)
-    annotate_p_values(ax, mann_whitney_p_values, y_pos=4)
-    ax.set_xlabel("Patient Mode Score (Standardized)")
-    ax.set_ylabel("PCA Score")
-    # ax.legend(title="Arrhythmic Composite", loc="lower center")
-
-    plt.tight_layout()
-    if not os.path.exists("./PCA_Results_final_height/figures"):
-        os.makedirs("./PCA_Results_final_height/figures")
-    plt.savefig("./PCA_Results_final_height/figures/arrhythmia_mode_comparison.svg")
-    plt.show()
+        # Plotting
+        plt.rcParams.update({"font.size": 14})
+        fig, ax = plt.subplots(figsize=(12, 6))
+        sns.boxplot(
+            data=clinical_data_pca_stacked,
+            x="mode",
+            y="score",
+            hue="any_fibrosis",
+            ax=ax,
+            palette="Set2",
+        )
 
 
+        ax.set_ylim(-3, 5)
+        annotate_p_values(ax, mann_whitney_p_values, y_pos=4)
+        ax.set_xlabel("Patient Mode Score (Standardized)")
+        ax.set_ylabel("PCA Score")
+        # ax.legend(title="Any Fibrosis", loc="upper center")
+        ax.legend(
+            title="Any Fibrosis",
+            loc="upper center",
+            bbox_to_anchor=(0.5, 1.2),  # adjust vertical offset as needed
+            ncol=2  # layout legend in a row if many labels
+        )
+
+        plt.tight_layout()
+        PCA_folder = PCA_SCORES_CSV.rsplit('/', 1)[0]
+        figure_path = os.path.join(PCA_folder, "figures")
+        if not os.path.exists(figure_path):
+            os.makedirs(figure_path)
+        plt.savefig(os.path.join(figure_path, "fibrosis_mode_comparison.svg"))
+        plt.show()
 
 
 ################# ILR ####################
